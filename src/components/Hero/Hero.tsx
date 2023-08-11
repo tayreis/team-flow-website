adb5674ac9a0c8c3bce137cdfe695699fadd6658
tayreis <taynareis07@gmail.com>
2023-08-10 23:36:35 -0300

    creating hero section

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,97 @@
+import  { styled }  from "styled-components"
+import ButtonPromo from "./ButtonPromo"
+import GraphImg from "./GraphImg"
+import PartnersImg from "./Partners"
+
+
+const HeroContainer = styled.section `
+    padding-top: 100px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    
+`
+const FirstDiv = styled.div `
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 24px;
+    
+`
+
+const HeroTitle = styled.h1 `
+    font-size: var( --title-primary);
+    color: var(--color-txt-primary);
+    text-align: center;
+    
+`
+
+const Paragraph = styled.p `
+    text-align: center;
+`
+
+const InputDiv = styled.div `
+    background-color: white;
+    margin-bottom: 48px;
+`
+
+const Input = styled.input `
+    padding: 18px 100px 18px 24px ;
+    /*border: #97A5B5 solid 1px;*/
+    border-radius: var(--border-radius-btn);
+    border: var(--color-txt-secondary) solid .2px;
+`
+
+const InputButton = styled.button `
+    padding: 18px 24px;
+    border: none;
+    color: white;
+    font-weight: bold;
+    background-color: var(--color-btn);
+    border-radius: var(--border-radius-btn);
+    cursor: pointer;
+`
+
+const PartnersDiv = styled.div `
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 24px;
+    padding: 100px 0;
+
+    > p > span {
+        font-weight: bold;
+    }
+   
+
+`
+
+
+export default function Hero() {
+    return (
+        
+        <HeroContainer id="circle">
+
+                <FirstDiv>
+                    <ButtonPromo/>
+                    <HeroTitle>Manage the team <br /> everyone wants to be on</HeroTitle>
+                    <Paragraph>Simple platform that lets you master what great managers do best,<br />
+                        as develop trust, collaborate, and drive team performance.
+                    </Paragraph>
+                    <InputDiv>
+                        <Input type="email" name="email" id="email" placeholder="name@yourcompany.com" />
+                        <InputButton type="submit">Try it free</InputButton>
+                    </InputDiv>
+                </FirstDiv>
+                <>
+                <GraphImg/>
+                </>
+                <PartnersDiv>
+                    <p>TRUSTED BY OVER <span>10.000+</span> WORLD’S BEST TEAMS</p>
+                    <PartnersImg/>
+                </PartnersDiv>
+
+            </HeroContainer>
+           
+    )
+}
---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+import Hero from "../Hero/Hero";
+import  { styled }  from "styled-components"
+
+
+const StyledMain = styled.main `
+   
+`
+
+export default function Main() {
+    return (
+        <main>
+            <Hero/>
+        </main>
+    )
+}
