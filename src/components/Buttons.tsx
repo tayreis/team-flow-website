f071b172e37aa6d67cd27d3c2ddabc1df2d2300b
tayreis <taynareis07@gmail.com>
2023-08-10 23:36:35 -0300

    creating hero section

---
+++ src/components/Buttons.tsx
@@ -0,0 +1,11 @@
+import ButtonLogin from "./ButtonLogin";
+import ButtonStarted from "./ButtonStarted";
+
+export default function Buttons() {
+    return (
+        <div>
+            <ButtonLogin/>
+            <ButtonStarted/>
+        </div>
+    )
+}
